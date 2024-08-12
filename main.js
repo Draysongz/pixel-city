306e8fbaf2073001936dfce09b1bdf3967aa2e48
draysongz <dharmiegra4@gmail.com>
2024-08-12 16:01:44 +0100

    done

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ window.addEventListener("popstate", router);
 
             if (viewport) {
                 viewport.content = "initial-scale=0.1";
-                viewport.content = "width=700";
+                viewport.content = "width=100%";
             }
         }
 
