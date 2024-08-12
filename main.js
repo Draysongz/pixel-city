4a2d9dd1b0151547adde2ffe6ef9262ab8d485bb
draysongz <dharmiegra4@gmail.com>
2024-08-12 16:00:51 +0100

    done

--- main.js
+++ main.js
@@ -43,3 +43,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Handle back/forward navigation
 window.addEventListener("popstate", router);
+  function zoomOutMobile() {
+            var viewport = document.querySelector('meta[name="viewport"]');
+
+            if (viewport) {
+                viewport.content = "initial-scale=0.1";
+                viewport.content = "width=700";
+            }
+        }
+
+        zoomOutMobile();
